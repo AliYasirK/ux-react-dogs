90f2079343be4e75886224a70ce08f52f385e999
Yasir <Yasir.A.Kulane@gmail.com>
2020-08-31 02:19:18 -0500

    it's 2am and I'm really tired

--- src/App.js
+++ src/App.js
@@ -3,41 +3,54 @@ import Dog from "./components/Dog";
 import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  let dogs = [
-    {
-      name: "Scruffles",
-      type: "Chihuahua",
-    },
-    {
-      name: "Snarf",
-      type: "Border Collie",
-    },
-    {
-      name: "Wookie",
-      type: "English Sheepdog",
-    },
-  ];
-
-  function pickNewFavorite() {
-    let newDogIndex = Math.floor(Math.random() * dogs.length);
-    return dogs[newDogIndex];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs: [
+        {
+          name: "Scruffles",
+          type: "Chihuahua",
+        },
+        {
+          name: "Snarf",
+          type: "Border Collie",
+        },
+        {
+          name: "Wookie",
+          type: "English Sheepdog",
+        },
+      ],
+      favoriteDog: () => {
+        let newDogIndex = Math.floor(Math.random() * this.state.dogs.length);
+        return this.state.dogs[newDogIndex];
+      },
+    }; // state
   }
 
-  let favoriteDog = pickNewFavorite();
+  pickNewFav() {
+    let dogLen = this.state.dogs.length();
+    let newDogIndex = Math.floor(Math.random() * dogLen);
+    this.setState({favoriteDog : this.state.dogs[newDogIndex]}); 
+  }
 
-  return (
-    <div className="dogs">
-      <Header numDogs={dogs.length} />
-      <h2>My Dogs</h2>
-      <div>
-        {dogs.map((dog) => (
-          <Dog dog={dog} />
-        ))}
+  render() {
+    return (
+      <div className="dogs">
+        <Header numDogs={this.state.dogs.length} />
+        <h2>My Dogs</h2>
+        <div>
+          {this.state.dogs.map((dog) => (
+            <Dog dog={dog} />
+          ))}
+        </div>
+        <h2>My Favorite dog is {this.state.favoriteDog().name}</h2>
+        <button onClick={this.pickNewFav}>
+          Pick New Favourite
+        </button>
       </div>
-      <h2>My Favorite dog is {favoriteDog.name}</h2>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
